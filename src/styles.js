b2f153cabf7d1640a17f06cf8379ef60c0bc32e3
bradcgarner <brad@bradgarner.com>
2018-07-16 14:04:14 -0400

    added notes in styles

--- src/styles.js
+++ src/styles.js
@@ -4,25 +4,29 @@ const { selectPalette } = require('./palettes');
 
 const createStyle = input => {
   const defaultGeneral = {
-    opacityBackground:      .2,
+    fill:                   true,
+    opacityBackground:      .1, 
+    // 0.05: faint on white, barely visible over gray, 
+    // 0.1: faint over white and gray (good for all colors over white)
+    // 0.2: prominent but translucent over white and gray (good for dark colors over gray)
+    // 1 is solid, 0.2:, 
     opacityBackgroundHover: .4,
     opacityBorder:          1,
     opacityBorderHover:     1,
     opacityPoint:           1,
     opacityPointHover:      1,
     opacityPointBackgroundHover: 1,
-    fill:                   true,
   
     lineTension:            0.5, // over 0.5 seems bulbous, 0 is angular
     bezierCurve:            true,
     bezierCurveTension:     0.5,
 
     borderCapStyle:         'butt',
-    borderDash:             [],
+    borderDash:             [], // [10,10] => ok, [20,20] => long dash, long gap, [5,20] => short dash, long gap
     borderDashOffset:       0.0,
     borderJoinStyle:        'miter',
-    borderWidth:            1,  
-    pointBorderWidth:       1,
+    borderWidth:            1,  // This is the LINE. 1: general all-purpose, 3: very thick line
+    pointBorderWidth:       1,  // 1: general all-purpose, 3: big dots
     pointHoverRadius:       5,
     pointHoverBorderWidth:  2,
     pointRadius:            1,
